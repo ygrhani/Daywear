980380261826b994d54f6035ad2930f76dea02bd
ygrhani <121520744+ygrhani@users.noreply.github.com>
2023-03-09 21:56:28 +0300

    category

---
+++ Daywear/ClothingListCVCell.swift
@@ -0,0 +1,24 @@
+//
+//  ClothingListCVCell.swift
+//  Daywear
+//
+//  Created by Ann Prudnikova on 27.02.23.
+//
+
+import UIKit
+import FirebaseDatabase
+
+class ClothingListCVCell: UICollectionViewCell {
+    
+    @IBOutlet weak var categoryImage: UIImageView!
+    @IBOutlet weak var categoryName: UILabel!
+    
+    var menuCategory: ClothingList? {
+        didSet {
+            categoryName.text = menuCategory?.title
+            if let image = menuCategory?.imageName {
+                categoryImage.image = UIImage(named: image)
+            }
+        }
+    }
+}
